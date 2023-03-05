e1fe6b41548137537eb4be93029780ff37cd1872
Artem <artemzhdanov1299@gmail.com>
2023-03-06 01:47:54 +0200

    Add project files.

---
+++ 02.03hm/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._03hm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ex2
+            //string Str2 = "zxc cvz zxcc czxz cvbt";
+            //string Pattern2 = @"([a])(\w{2})([a])";
+            //Regex Reg2 = new Regex(Pattern2);
+            //Match M2 = Reg2.Match(Str2);
+            //while (M2.Success)
+            //{
+            //    Console.WriteLine(M2.Groups[0]);
+            //    M2 = M2.NextMatch();
+            //}
+            #endregion
+            #region ex1
+            //string Str1 = "zxc cvz zxcc czxz cvbt";
+            //string Pattern1 = @"([a])(\w)([b])";
+            //Regex Reg1 = new Regex(Pattern1);
+            //Match M1 = Reg1.Match(Str1);
+            //while (M1.Success)
+            //{
+            //    Console.WriteLine(M1.Groups[0]);
+            //    M1 = M1.NextMatch();
+            //}
+            #endregion
+
+            #region ex3
+            //string Str3 = "zxc cvz zxcc czxz cvbt";
+            //string Pattern3 = @"([a])([b])(\w)([a])";
+            //Regex Reg3 = new Regex(Pattern3);
+            //Match M3 = Reg3.Match(Str3);
+            //while (M3.Success)
+            //{
+            //    Console.WriteLine(M3.Groups[0]);
+            //    M3 = M3.NextMatch();
+            //}
+            #endregion
+
+            #region ex4
+            //string Str4 = "zx xz xc ce rez xzc";
+            //string Pattern4 = @"([a])([b]+)([a])";
+            //Regex Reg4 = new Regex(Pattern4);
+            //Match M4 = Reg4.Match(Str4);
+            //while (M4.Success)
+            //{
+            //    Console.WriteLine(M4.Groups[0]);
+            //    M4 = M4.NextMatch();
+            //}
+            #endregion
+            #region ex5
+            //string Str5 = "zxc cvz zxcc cxezxz cvbt;
+            //string Pattern5 = @"([a])([b]+)?([a])";
+            //Regex Reg5 = new Regex(Pattern5);
+            //Match M5 = Reg5.Match(Str5);
+            //while (M5.Success)
+            //{
+            //    Console.WriteLine(M5.Groups[0]);
+            //    M5 = M5.NextMatch();
+            //}
+            #endregion
+
+            #region ex6
+            //string Str6 = "zxc cvz zxcc cxezxz cvbt";
+            //string Pattern6 = @"([a])([b])?([a])";
+            //Regex Reg6 = new Regex(Pattern6);
+            //Match M6 = Reg6.Match(Str6);
+            //while (M6.Success)
+            //{
+            //    Console.WriteLine(M6.Groups[0]);
+            //    M6 = M6.NextMatch();
+            //}
+            #endregion
+
+            #region ex7
+            //string Str7 = "zxc cvz zxcc cxezxz cvbt";
+            //string Pattern7 = @"([a])([b]+)?([a])";
+            //Regex Reg7 = new Regex(Pattern7);
+            //Match M7 = Reg7.Match(Str7);
+            //while (M7.Success)
+            //{
+            //    Console.WriteLine(M7.Groups[0]);
+            //    M7 = M7.NextMatch();
+            //}
+            #endregion
+            #region ex8
+            //string Str8 = "zxc cvz zxcc cxezxzzx cvbt";
+            //string Pattern8 = @"((ab)+)";
+            //Regex Reg8 = new Regex(Pattern8);
+            //Match M8 = Reg8.Match(Str8);
+            //while (M8.Success)
+            //{
+            //    Console.WriteLine(M8.Groups[0]);
+            //    M8 = M8.NextMatch();
+            //}
+            #endregion
+            #region ex7
+            //string Str7 = "zxc cvz zxcc cxezxzzx cvbt";
+            //string Pattern7 = @"([a])([b]+)?([a])";
+            //Regex Reg7 = new Regex(Pattern7);
+            //Match M7 = Reg7.Match(Str7);
+            //while (M7.Success)
+            //{
+            //    Console.WriteLine(M7.Groups[0]);
+            //    M7 = M7.NextMatch();
+            //}
+            #endregion
+        }
+    }
+}
